f8005df700f902d13d1dce4bd7fb07936728e84a
Gustavo Rodrigues <gustavorodriguesm@hotmail.com>
2021-05-04 22:05:50 -0300

    Adicionando permissao de GPS na primeira tela

--- app/src/main/java/com/example/projetopresencaaluno/MainActivity.java
+++ app/src/main/java/com/example/projetopresencaaluno/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.projetopresencaaluno;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.HashMap;
@@ -19,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        solicitaPermissaoLocalizacao();
         Button btnConectar = (Button) findViewById(R.id.btnConectar);
         EditText edtSenha = (EditText) findViewById(R.id.edtSenha);
         EditText edtRGM = (EditText) findViewById(R.id.edtRGM);
@@ -58,5 +66,13 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void solicitaPermissaoLocalizacao() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+
     }
 }
